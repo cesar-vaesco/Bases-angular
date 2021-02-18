7891ca5262286210d3c3e1af43f2e8367d9cda06
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-02-17 19:12:20 -0600

    Bases: métodos en el componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  titulo:string = 'Contador App';
-  numero:number = 10;
+  titulo: string = 'Contador App';
+  numero: number = 10;
+
+  sumar() {
+    this.numero += 1;
+  }
+  restar() {
+    this.numero -= 1;
+  }
 }
